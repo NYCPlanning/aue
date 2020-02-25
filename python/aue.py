b90f38321200cb54d83635e4445f3b26964b833e
mgraber <molly.graber@me.com>
2020-02-25 15:37:43 +0000

    Modify output formatting so each row is a possible lot combination

--- python/aue.py
+++ python/aue.py
@@ -14,19 +14,20 @@ converse = intersections.rename(columns={'t1':'t2','t2':'t1'})
 intersections_all = intersections.append(converse)
 print("Loaded intersection data and appended with converse")
 
+# Pivot intersection table to create matrix
+print("Pivoting to create possibility matrix...")
+p_df = intersections_all.pivot(index='t2', columns='t1', values='intersection')
+
 # Create BBL-Index lookup, as well as BBL set for tree-creation
-bbl_list = intersections['t1'].unique().tolist()
+bbl_list = list(p_df.index.values)
 bbl_lookup = {}
 for idx, bbl in enumerate(bbl_list):
     bbl_lookup[idx] = bbl
+
 bbl_set = set(bbl_list)
 bbl_index_set = set(bbl_lookup.keys())
 print("Minimum index: ", min(bbl_index_set))
 print("Number of unique input BBLs: ", len(bbl_set))
-
-# Pivot intersection table to create matrix
-print("Pivoting to create possibility matrix...")
-p_df = intersections_all.pivot(index='t2', columns='t1', values='intersection')
 
 # Convert to numpy array and set diagonals to nan
 p = p_df.to_numpy()
@@ -36,25 +37,24 @@ print("Top left corner of possibility matrix: \n", p[0:12,0:12])
 # Find trees
 print("Finding trees...")
 results = all_trees(bbl_index_set, p)
+print("Complete.")
 
 # Convert indecies back to BBLs
-def get_bbls(results):
-    results['bbls'] = [bbl_lookup[idx] for idx in results['lot_idx']]
-map(get_bbls, results)
+map([bbl_lookup[idx] for idx in list(result['lots_idx'])], results)
 
 # Find best- and worst-case scenarios
 best = max(item['number'] for item in results)
 print("Max number of units is %d" % best)
-best_bbls = list(filter(lambda d: d['number'] == best, results))
-with open('output/best.csv', 'wb') as output_file:
-    dict_writer = csv.DictWriter(output_file, ['number','lots_idx','bbls'])
-    dict_writer.writeheader()
-    dict_writer.writerows(best_bbls)
+best_subset = list(filter(lambda d: d['number'] == best, results))
+best_bbls = [d['bbls'] for d in best_subset]
+with open('output/best.csv', 'w', newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(best_bbls)
 
 worst = min(item['number'] for item in results)
 print("Min number of units is %d" % worst)
-worst_bbls = list(filter(lambda d: d['number'] == worst, results))
-with open('output/worst.csv', 'wb') as output_file:
-    dict_writer = csv.DictWriter(output_file, ['number','lots_idx','bbls'])
-    dict_writer.writeheader()
-    dict_writer.writerows(worst_bbls)
+worst_subset = list(filter(lambda d: d['number'] == worst, results))
+worst_bbls = [d['bbls'] for d in worst_subset]
+with open('output/worst.csv', 'w', newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(worst_bbls)
