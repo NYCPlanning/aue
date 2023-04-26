6d827e90bee80840be5f0c4ce1d47dc852b02ec4
Damon McCullough <damonmcc1391@gmail.com>
2023-04-25 21:30:03 -0400

    update main script and outputs it generated

--- aue.sh
+++ aue.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
+set -e
 source config.sh
 
-echo "Buffering lots and calculating intersections"
+echo "Buffering lots and calculating intersections ..."
 
 mkdir -p data
 
-psql $RECIPE_ENGINE -c "\COPY (SELECT t1.bbl AS t1, t2.bbl AS t2,
+psql $RECIPE_ENGINE --set ON_ERROR_STOP=1 -c "\COPY (SELECT t1.bbl AS t1, t2.bbl AS t2,
   (CASE WHEN ST_Intersects(ST_buffer(t1.wkb_geometry, 500), t2.wkb_geometry) then 0 else 1 END) as intersection 
-  FROM aue_lots.\"20200731_full\" AS t1
-  INNER JOIN aue_lots.\"20200731_full\" AS t2 on (t1.bbl != t2.bbl))
-  TO STDOUT DELIMITER ',' CSV HEADER;" > data/intersection.csv
+  FROM dcp_proximity_establishments.\"20230425\" AS t1
+  INNER JOIN dcp_proximity_establishments.\"20230425\" AS t2 on (t1.bbl != t2.bbl))
+  TO "data/intersection.csv" DELIMITER ',' CSV HEADER;"
 
-echo "Beginning python calculations"
+echo "Beginning python calculations ..."
 
 docker run --rm\
     -v `pwd`:/home/aue\
     -w /home/aue\
     --env-file .env\
     nycplanning/cook:latest bash -c "pip3 install -r python/requirements.txt; python3 python/aue.py"
+
+echo "Done!"
