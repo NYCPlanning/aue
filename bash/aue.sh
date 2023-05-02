27deba74113e1ae6af0a4a4b4650935c5f46e4e2
Damon McCullough <damonmcc1391@gmail.com>
2023-05-02 14:27:45 +0000

    .env is now more coupled to dev container

--- bash/aue.sh
+++ bash/aue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-source $(dirname "$0")/utils.sh
-set_env .env
+# source $(dirname "$0")/utils.sh
+# set_env .env
 
 echo "Buffering lots and calculating intersections for version ${INPUT_VERSION} ..."
 
