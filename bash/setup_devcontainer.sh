b30d8182ad88374d1121658bbccaf528b836bda9
Damon McCullough <damonmcc1391@gmail.com>
2023-05-02 03:09:11 +0000

    more printing during dev container setup

--- bash/setup_devcontainer.sh
+++ bash/setup_devcontainer.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 #
-# Sets up enviroment for dev in local devcontainer.
+# Sets up enviroment for dev in local dev container.
 set -e
+
+echo "Setting up dev container ..."
 
 # Skip when called in a github action workflow
 if [[ ${CI} != "true" ]]; then
+	echo "Adding local SSH keys ..."
 	# Add local SSH private keys in order to push to github from the dev container
 	ssh-add
 fi
+
+echo "Done setting up dev container"
