d4854bee3221e0d4e26984b3c60c002a751a9688
Damon McCullough <damonmcc1391@gmail.com>
2023-05-10 02:45:11 +0000

    move a model from staging to intermediate

--- aue_dbt/models/final/tax_lot_best_times.sql
+++ aue_dbt/models/final/tax_lot_best_times.sql
@@ -3,7 +3,7 @@ with tax_lot_geometries as (
 ),
 
 borough_best_times as (
-    select * from {{ ref('stg_dcp__borough_times') }}
+    select * from {{ ref('borough_best_times') }}
 ),
 
 tax_lot_best_time as (
